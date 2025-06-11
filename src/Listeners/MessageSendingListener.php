aa04a8a1c53f07d84fd0f40410e636b78cb0f225
prajwal89 <22263436+prajwal89@users.noreply.github.com>
2025-06-11 22:31:38 +0530

    refactor store loginc

--- src/Listeners/MessageSendingListener.php
+++ src/Listeners/MessageSendingListener.php
@@ -9,6 +9,7 @@ use Illuminate\Mail\Events\MessageSending;
 use Illuminate\Support\Facades\DB;
 use Prajwal89\EmailManagement\Enums\RecipientType;
 use Prajwal89\EmailManagement\Models\EmailLog;
+use Prajwal89\EmailManagement\Services\EmailLogService;
 use Prajwal89\EmailManagement\Services\HeadersManager;
 
 /**
@@ -23,62 +24,12 @@ class MessageSendingListener
 
         $message = $event->message;
 
-        $headers = $message->getHeaders();
-
-        try {
-            DB::beginTransaction();
-
-            $emailLog = EmailLog::query()->create([
-                'message_id' => $headersManager->getMessageId(),
-
-                // todo: from can have multiple values also
-                'from' => $message->getFrom()[0]->getAddress(),
-
-                'sendable_type' => $headersManager->getSendable()['type'],
-                'sendable_id' => $headersManager->getSendable()['id'],
-
-                'receivable_type' => $headersManager->getReceivable()['type'],
-                'receivable_id' => $headersManager->getReceivable()['id'],
-
-                'email_variant_id' => $headersManager->getEmailVariantId(),
-
-                'subject' => $message->getSubject(),
-                'html' => $message->getHtmlBody(),
-                'text' => $message->getTextBody(),
-                'headers' => $headers->toArray(),
-                'context' => $headersManager->getEventContext(),
-
-                // todo: add this
-                // 'mailer' => $mailerName,
-                // 'transport' => $transportName,
-            ]);
-
-            foreach ($message->getTo() as $i => $to) {
-                $emailLog->recipients()->create([
-                    'type' => RecipientType::TO,
-                    'email' => $to->getAddress(),
-                ]);
-            }
-
-            foreach ($message->getCc() as $i => $cc) {
-                $emailLog->recipients()->create([
-                    'type' => RecipientType::CC,
-                    'email' => $cc->getAddress(),
-                ]);
-            }
-
-            foreach ($message->getBcc() as $i => $bcc) {
-                $emailLog->recipients()->create([
-                    'type' => RecipientType::BCC,
-                    'email' => $bcc->getAddress(),
-                ]);
-            }
-
-            DB::commit();
-        } catch (Exception $e) {
-            DB::rollBack();
-
-            throw $e;
+        // for the emails that are not sent from the handler
+        // todo: where to do content modification
+        if ($headersManager->getMessageId() == null) {
+            $headersManager->createMessageId();
         }
+
+        EmailLogService::store($message);
     }
 }
--- src/Services/EmailLogService.php
+++ src/Services/EmailLogService.php
@@ -5,9 +5,79 @@ declare(strict_types=1);
 namespace Prajwal89\EmailManagement\Services;
 
 use Prajwal89\EmailManagement\Models\EmailLog;
+use Symfony\Component\Mime\Email;
+use Exception;
+use Illuminate\Mail\Events\MessageSending;
+use Illuminate\Support\Facades\DB;
+use Prajwal89\EmailManagement\Enums\RecipientType;
+use Prajwal89\EmailManagement\Services\HeadersManager;
+
 
 class EmailLogService
 {
+    public static function store(Email $message): EmailLog
+    {
+        $headersManager = new HeadersManager($message);
+
+        try {
+            DB::beginTransaction();
+
+            $emailLog = EmailLog::query()->create([
+                'message_id' => $headersManager->getMessageId(),
+
+                // todo: from can have multiple values also
+                'from' => $message->getFrom()[0]->getAddress(),
+
+                'sendable_type' => $headersManager->getSendable()['type'],
+                'sendable_id' => $headersManager->getSendable()['id'],
+
+                'receivable_type' => $headersManager->getReceivable()['type'],
+                'receivable_id' => $headersManager->getReceivable()['id'],
+
+                'email_variant_id' => $headersManager->getEmailVariantId(),
+
+                'subject' => $message->getSubject(),
+                'html' => $message->getHtmlBody(),
+                'text' => $message->getTextBody(),
+                'headers' => $message->getHeaders()->toArray(),
+                'context' => $headersManager->getEventContext(),
+
+                // todo: add this
+                // 'mailer' => $mailerName,
+                // 'transport' => $transportName,
+            ]);
+
+            foreach ($message->getTo() as $i => $to) {
+                $emailLog->recipients()->create([
+                    'type' => RecipientType::TO,
+                    'email' => $to->getAddress(),
+                ]);
+            }
+
+            foreach ($message->getCc() as $i => $cc) {
+                $emailLog->recipients()->create([
+                    'type' => RecipientType::CC,
+                    'email' => $cc->getAddress(),
+                ]);
+            }
+
+            foreach ($message->getBcc() as $i => $bcc) {
+                $emailLog->recipients()->create([
+                    'type' => RecipientType::BCC,
+                    'email' => $bcc->getAddress(),
+                ]);
+            }
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            throw $e;
+        }
+
+        return $emailLog;
+    }
+
     // we can invoke events from here
     public static function update(EmailLog $emailLog, array $attributes)
     {
