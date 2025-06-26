cc12658fc39ac741dbf062e3fef2777663105ed0
prajwal89 <22263436+prajwal89@users.noreply.github.com>
2025-06-26 15:32:22 +0530

    remove old tests

--- tests/Feature/EmailHeadersTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-it('has mailer hash header', function (): void {})->todo();
-
-it('has mailer list unsubscribe headers', function (): void {})->todo();
--- tests/Feature/SubscriptionTest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-it('can unsubscribe the user from email', function (): void {})->todo();
