13a9bede6a82050aabef93ebdaf18661300d1124
prajwal89 <22263436+prajwal89@users.noreply.github.com>
2025-07-04 21:58:50 +0530

    HandleNewEmailReceivedJob

--- src/Listeners/NewEmailReceivedListener.php
+++ src/Listeners/NewEmailReceivedListener.php
@@ -8,10 +8,9 @@ use DirectoryTree\ImapEngine\Laravel\Events\MessageReceived;
 use DirectoryTree\ImapEngine\MessageInterface;
 use Illuminate\Support\Facades\Log;
 use Prajwal89\EmailManagement\BounceParser;
+use Prajwal89\EmailManagement\Dtos\BounceDataDto;
+use Prajwal89\EmailManagement\Jobs\HandleNewEmailReceivedJob;
 use Prajwal89\EmailManagement\Models\EmailLog;
-
-// todo this should be job as exception here will break the command execution
-// and it will be more easy to track jobs
 
 /**
  * Enhanced email listener that handles bounces, replies, and conversation threading
@@ -21,18 +20,10 @@ use Prajwal89\EmailManagement\Models\EmailLog;
 class NewEmailReceivedListener
 {
 
-    public function handle(MessageReceived $event): void
+    public function handle(MessageReceived $event)
     {
         $message = $event->message;
 
-        $parser = new BounceParser;
-
-        // we can mock this to test
-        $result = $parser->parse($message->__toString());
-
-        Log::info('Message', [
-            'message' => $message->__toString(),
-            'result' => $result,
-        ]);
+        dispatch(new HandleNewEmailReceivedJob($message));
     }
 }
